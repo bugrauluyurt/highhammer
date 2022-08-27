2e3b52ce63502208d9b55698e2e1f173cd429255
Bugra Uluyurt <bugra.u@gmail.com>
2022-08-27 16:25:03 -0400

    chore: node modules check completed

---
+++ tools/dev-scripts/cmd-check-node-modules.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const fs = require('fs');
+
+const nodeModulePath = path.resolve(__dirname, '../../node_modules');
+
+const isNodeModuleDirEmpty = () => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(nodeModulePath, function(err, files) {
+      if (err) {
+        reject(err);
+      } else {
+        if (!files.length) {
+            // directory appears to be empty
+            reject(err);
+        }
+        resolve();
+      }
+    });
+  })
+}
+
+const run = () => {
+  isNodeModuleDirEmpty()
+    .then(() => {
+      console.log('[Highhammer] node_modules checked successfully.')
+      process.exit(0);
+    })
+    .catch(() => {
+      console.error('[Highhammer] Please run <npm run setup> to install node modules.')
+      process.exit(1);
+    })
+}
+
+run();
+
+
