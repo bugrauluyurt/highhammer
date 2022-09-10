8a3883a76c84a11ad7cc26910deb29e5c75f7c8f
Bugra Uluyurt <bugra.u@gmail.com>
2022-09-10 15:50:31 -0400

    chore: env file check added

--- tools/dev-scripts/cmd-check-dependencies.js
+++ tools/dev-scripts/cmd-check-dependencies.js
@@ -19,19 +19,38 @@ const checkNvmVersion = () => {
   })
   .then((nvmNodeVersion) => ({ nvmNodeVersion }))
   .catch((error) => ({ nvmNodeError: error }))
-}
+};
 
 const checkNodeVersion = () => exec("node -v", { shell: defaultShell })
   .then((output) => ({ node: true, nodeVersion: output.stdout }))
-  .catch((output) => ({ node: false, nodeError: output.stderr }))
+  .catch((output) => ({ node: false, nodeError: output.stderr }));
 
 const checkDocker = () => exec("docker --version", { shell: defaultShell })
   .then(() => ({ docker: true }))
-  .catch((output) => ({ docker: false, dockerError: output.stderr }))
+  .catch((output) => ({ docker: false, dockerError: output.stderr }));
+
+const checkEnvFiles = () => {
+  const hostEnvExists = fs.existsSync(path.resolve(__dirname, '../../config/env/.env.host'));
+  const qaEnvExists = fs.existsSync(path.resolve(__dirname, '../../config/env/.env.qa'));
+  const devEnvExists = fs.existsSync(path.resolve(__dirname, '../../config/env/.env.dev'));
+  if (!hostEnvExists || !qaEnvExists || !devEnvExists) {
+    if (!hostEnvExists) {
+      console.error('[Highhammer] Please put .env.host file inside your ./config/env/ folder.');
+    }
+    if (!qaEnvExists) {
+      console.error('[Highhammer] Please put .env.qa file inside your ./config/env/ folder.');
+    }
+    if (!devEnvExists) {
+      console.error('[Highhammer] Please put .env.dev file inside your ./config/env/ folder.');
+    }
+    console.error('[Highhammer] You can copy the template .env.${HOST_ENV} file from ./config/env folder for each environment.');
+    process.exit(1);
+  }
+}
 
 const checkCertificates = () => {
-  const keyExists = fs.existsSync(path.resolve(__dirname, '../../certificates/localhost.key'));
-  const crtExists = fs.existsSync(path.resolve(__dirname, '../../certificates/localhost.crt'));
+  const keyExists = fs.existsSync(path.resolve(__dirname, '../../config/certificates/localhost.key'));
+  const crtExists = fs.existsSync(path.resolve(__dirname, '../../config/certificates/localhost.crt'));
   const certificates = { keyExists, crtExists };
   if (!certificates.crtExists || !certificates.keyExists) {
     if (!certificates.crtExists) {
@@ -44,8 +63,8 @@ const checkCertificates = () => {
   }
 }
 
-const printNodeVersionError = () => {
-  console.error(`[Highhammer] Please install the correct node version (${data.nvmNodeVersion})`);
+const printNodeVersionError = (data) => {
+  console.error(`[Highhammer] Please use the correct node version -> ${data?.nvmNodeVersion}`);
 }
 
 const printDockerError = () => {
@@ -53,6 +72,7 @@ const printDockerError = () => {
 }
 
 const checkDependencies = () => {
+  checkEnvFiles();
   checkCertificates();
 
   let request = [
@@ -70,14 +90,14 @@ const checkDependencies = () => {
   }
 
   if (isOnlyDocker) {
-    request = [checkDocker()]
+    request = [checkDocker()];
   }
 
   Promise.all(request)
     .then((response) => {
       const data = response.reduce((acc, partial) => {
         return {...acc, ...partial};
-      }, {})
+      }, {});
 
       // Only Docker
       if (isOnlyDocker) {
@@ -91,7 +111,7 @@ const checkDependencies = () => {
       const isNodeVersionCompatible = (() => data?.node && (data?.nodeVersion === data?.nvmNodeVersion))();
       if (isOnlyNode) {
         if (!isNodeVersionCompatible) {
-          printNodeVersionError();
+          printNodeVersionError(data);
         }
         return process.exit(!isNodeVersionCompatible ? 1 : 0);
       }
@@ -100,17 +120,17 @@ const checkDependencies = () => {
       const areDependenciesInstalled = isNodeVersionCompatible && data.docker;
       if (!areDependenciesInstalled) {
         if (!isNodeVersionCompatible) {
-          printNodeVersionError();
+          printNodeVersionError(data);
         }
         if (!data.docker) {
-          printDockerError()
+          printDockerError();
         }
         return process.exit(1);
       }
-      console.log('[Highammer] Dependency check successful.')
+      console.log('[Highammer] Dependency check successful.');
       return process.exit(0);
-    })
-}
+    });
+};
 
 checkDependencies();
 
