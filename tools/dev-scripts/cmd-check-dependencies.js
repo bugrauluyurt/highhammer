e2ca19eb410788884ac714c95295654384cbfdb0
Bugra Uluyurt <bugra.u@gmail.com>
2022-09-10 13:41:56 -0400

    chore: certificate checking added

--- tools/dev-scripts/cmd-check-dependencies.js
+++ tools/dev-scripts/cmd-check-dependencies.js
@@ -29,6 +29,21 @@ const checkDocker = () => exec("docker --version", { shell: defaultShell })
   .then(() => ({ docker: true }))
   .catch((output) => ({ docker: false, dockerError: output.stderr }))
 
+const checkCertificates = () => {
+  const keyExists = fs.existsSync(path.resolve(__dirname, '../../certificates/localhost.key'));
+  const crtExists = fs.existsSync(path.resolve(__dirname, '../../certificates/localhost.crt'));
+  const certificates = { keyExists, crtExists };
+  if (!certificates.crtExists || !certificates.keyExists) {
+    if (!certificates.crtExists) {
+      console.error('[Highhammer] Please generate a localhost.crt file inside your ./config/certificates/ folder.');
+    }
+    if (!certificates.keyExists) {
+      console.error('[Highhammer] Please generate a localhost.key file inside your ./config/certificates/ folder.');
+    }
+    process.exit(1);
+  }
+}
+
 const printNodeVersionError = () => {
   console.error(`[Highhammer] Please install the correct node version (${data.nvmNodeVersion})`);
 }
@@ -38,20 +53,26 @@ const printDockerError = () => {
 }
 
 const checkDependencies = () => {
+  checkCertificates();
+
   let request = [
     checkNodeVersion(),
     checkNvmVersion(),
     checkDocker()
   ];
+
   const { only } = getArgs();
   const isOnlyNode = only === 'node';
   const isOnlyDocker = only === 'docker';
+
   if (isOnlyNode) {
     request = [checkNodeVersion(), checkNvmVersion()]
   }
+
   if (isOnlyDocker) {
     request = [checkDocker()]
   }
+
   Promise.all(request)
     .then((response) => {
       const data = response.reduce((acc, partial) => {
