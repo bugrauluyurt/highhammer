c6aff0cd5c39f01aeddd9813ff39163b85a21827
Bugra Uluyurt <bugra.u@gmail.com>
2022-10-23 22:34:55 -0400

    chore: versioning changed

--- tools/dev-scripts/cmd-generate-calver.js
+++ tools/dev-scripts/cmd-generate-calver.js
@@ -9,10 +9,10 @@ const processArguments = process.argv.slice(2);
 const PROJECT_NAME = processArguments[0];
 const CALVER_FORMAT = 'YYYY.0M.0D.MINOR';
 
-const getProjectPackageJsonPath = (projectName) => path.resolve(__dirname, `../../apps/${projectName}/package.json`);
+const getVersionsJsonPath = () => path.resolve(__dirname, `../../versions.json`);
 
-const getProjectPackageJson = (projectName) => {
-  const json = fs.readFileSync(getProjectPackageJsonPath(projectName));
+const getVersionsJson = () => {
+  const json = fs.readFileSync(getVersionsJsonPath());
   return JSON.parse(json);
 }
 
@@ -31,7 +31,7 @@ const getCurrentCalverDate = (date = new Date(), format = 'yyyy.mm.dd') => {
 }
 
 const generateNextCalver = (projectName) => {
-  let versionCurrent = getProjectPackageJson(projectName)?.version || '';
+  let versionCurrent = getVersionsJson()[projectName] || '';
   if (versionCurrent === '0.0.0') {
     versionCurrent = '';
   }
@@ -49,18 +49,13 @@ const generateNextCalver = (projectName) => {
   return calver.inc(CALVER_FORMAT, versionCurrent, versionType);
 }
 
-const bumpProjectVersion = (projectName, nextCalver) => {
-  const projectPackageJson = {...getProjectPackageJson(projectName)};
-  projectPackageJson.version = nextCalver;
-  fs.writeFileSync(getProjectPackageJsonPath, JSON.stringify(projectPackageJson));
-}
-
 const run = () => {
   try {
     if (!PROJECT_NAME?.length) {
       throw Error('Please input a projectName as a cmd line argument.');
     }
     const nextCalver = generateNextCalver(PROJECT_NAME);
+    // @INFO: Log is for getting the stdout
     console.log(nextCalver);
     process.exit(0);
   } catch (e) {
