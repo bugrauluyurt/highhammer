fa49926a510b75e1cebbfbc35d131837d954b6bd
Bugra Uluyurt <bugra.u@gmail.com>
2022-10-23 22:10:31 -0400

    chore: project version generation fixed

--- tools/dev-scripts/cmd-bump-project-version.js
+++ tools/dev-scripts/cmd-bump-project-version.js
@@ -8,17 +8,17 @@ const processArguments = process.argv.slice(2);
 const PROJECT_NAME = processArguments[0];
 const VERSION = processArguments[1];
 
-const getProjectPackageJsonPath = (projectName) => path.resolve(__dirname, `../../apps/${projectName}/package.json`);
+const getVersionsJsonPath = () => path.resolve(__dirname, `../../versions.json`);
 
-const getProjectPackageJson = (projectName) => {
-  const json = fs.readFileSync(getProjectPackageJsonPath(projectName));
+const getVersionsJson = () => {
+  const json = fs.readFileSync(getVersionsJsonPath());
   return JSON.parse(json);
 }
 
 const bumpProjectVersion = (projectName, nextCalver) => {
-  const projectPackageJson = {...getProjectPackageJson(projectName)};
-  projectPackageJson.version = nextCalver;
-  fs.writeFileSync(getProjectPackageJsonPath(projectName), JSON.stringify(projectPackageJson, null, 4));
+  const versionsJson = {...getVersionsJson()};
+  versionsJson[projectName].version = nextCalver;
+  fs.writeFileSync(getVersionsJsonPath(), JSON.stringify(versionsJson, null, 4));
 }
 
 const run = () => {
