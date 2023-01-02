fa894ba335b64ae9ae6208f239605ee0dbc59cf7
Bugra Uluyurt <bugra.u@gmail.com>
2023-01-01 20:14:03 -0500

    chore: docker local repo tools completed

--- tools/dev-scripts/cmd-k8-test.js
+++ tools/dev-scripts/cmd-k8-test.js
@@ -1,6 +1,11 @@
+const path = require('path');
+const fs = require('fs');
 const util = require('node:util');
 const execPromise = util.promisify(require('node:child_process').exec);
 
+const PROJECTS_EXCLUDED = ['api-worker', 'client-app-e2e']
+
+const DOCKER_REGISTRY_URL = 'localhost'
 const DOCKER_REGISTRY_NAME = 'registry';
 const DOCKER_REGISTRY_PORT = 5020;
 const DOCKER_REGISTRY_VERSION = 2;
@@ -35,11 +40,13 @@ const isDockerLocalRegistryRunning = async () => {
 const runDockerLocalRegistry = async () => {
   const isRegistryRunning = await isDockerLocalRegistryRunning();
   const logSuccess = () => {
+    console.log('-------------------')
     console.log(`[Highhammer] Docker registry running on port ${DOCKER_REGISTRY_PORT}.`);
     const logMessageForTail = '[Highhammer] To tail the container logs you can run:\n $ docker logs -f registry';
     console.log(logMessageForTail);
     const logMessageForTermination = `[Highhammer] To stop the registry you can run:\n $ docker container stop ${DOCKER_REGISTRY_NAME} && docker container rm -v ${DOCKER_REGISTRY_NAME}`;
     console.log(logMessageForTermination);
+    console.log('-------------------')
   }
   if (isRegistryRunning) {
     logSuccess()
@@ -60,13 +67,75 @@ const runDockerLocalRegistry = async () => {
     });
 }
 
+const getLatestTagVersion = async () => {
+  return execPromise(`git describe --abbrev=0 --tags | tr -d v`)
+    .then((response) => {
+      if (response?.stderr?.length) {
+        console.log(response?.stderr);
+      } else {
+        const latestTagVersion = response.stdout
+        if (latestTagVersion.length && latestTagVersion.split('.').length === 3) {
+          return latestTagVersion
+        }
+        return '0.0.0'
+      }
+    })
+    .catch((e) => {
+      console.error('[Highhammer] An error occurred while getting the latest tag version. Returning the tag version as 0.0.0', e);
+      return '0.0.0'
+    });
+}
+
+const tagAndPushProjectImage = async (projectName, tag) => {
+  const cmdDockerBuild = `docker build -t ${tag} -f ./apps/${projectName}/Dockerfile ./apps/${projectName}`.replace(/\n/, '')
+  await execPromise(cmdDockerBuild)
+  await execPromise(`docker push ${tag}`)
+}
+
+const tagAndPushProjectImages = async () => {
+  const versions = getVersionsJson();
+  const latestTagVersion = await getLatestTagVersion();
+  const promiseBatch = Object.entries(versions)
+    .filter(([projectName]) => !PROJECTS_EXCLUDED.includes(projectName))
+    .map(([projectName, calVer]) => {
+      const imageName = projectName
+      const versionProject = calVer
+      const versionMonoRepo = latestTagVersion
+      const containerTag = `${DOCKER_REGISTRY_URL}:${DOCKER_REGISTRY_PORT}/${imageName}:${versionProject}-${versionMonoRepo}`
+      return tagAndPushProjectImage(projectName, containerTag)
+    });
+  return Promise.all(promiseBatch)
+    .then((response) => {
+      if (response?.stderr?.length) {
+        console.log(response?.stderr);
+      } else {
+        console.log(`[Highhammer] Project images are tagged and pushed successfully.`);
+      }
+    })
+    .catch((e) => {
+      console.error('[Highhammer] An error occurred while tagging and pushing project images.', e);
+      process.exit(1);
+    });
+}
+
 const buildProjects = async () => {
   return execPromise(`pnpm nx:build`)
     .then((response) => {
       if (response?.stderr?.length) {
         console.log(response?.stderr);
       } else {
-        console.log(`[Highhammer] Project are build successfully.`);
+        console.log(`[Highhammer] All projects are build successfully.`);
+        const versions = getVersionsJson();
+        // @INFO: Copy all the dist folder into the project's own folder
+        const promiseBatch = Object.keys(versions)
+          .filter((projectName) => {
+            return !PROJECTS_EXCLUDED.includes(projectName)
+          })
+          .map((projectName) => {
+            return execPromise(`rm -rf ./apps/${projectName}/dist && cp -r ./dist/apps/${projectName}/ ./apps/${projectName}/dist/`)
+              .catch(() => undefined)
+          })
+        return Promise.all(promiseBatch)
       }
     })
     .catch((e) => {
@@ -77,8 +146,12 @@ const buildProjects = async () => {
 
 const run = async () => {
   try {
+    console.log(`[Highhammer] Init->runDockerLocalRegistry()`);
     await runDockerLocalRegistry();
+    console.log(`[Highhammer] Init->buildProjects()`);
     await buildProjects()
+    console.log(`[Highhammer] Init->tagAndPushProjectImages()`);
+    await tagAndPushProjectImages()
   } catch (e) {
     console.log(e)
     process.exit(1)
